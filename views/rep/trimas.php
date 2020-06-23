c67ade309c5e928375e233b73c4f038983b1d30c
Pgans <mhosp.gan@gmail.com>
2020-06-23 16:02:26 +0700

    trimas3

--- views/rep/trimas.php
+++ views/rep/trimas.php
@@ -12,9 +12,9 @@ $this->title = 'ไตรมาส1';
 //$this->params['breadcrumbs'][] = ['label' => 'รายงาน', 'url' => ['rep/index']];
 //$this->params['breadcrumbs'][] = 'รายงานข้อมูลE-Claim แยกตามREP';
 ?>
-  <div class="col-md-12">
+  <div class="col-md-6">
                     <div class="panel panel-info">
-                        <div class="panel-heading"><i class="glyphicon glyphicon-user"></i>รายงานข้อมูลE-Claim ผู้ป่วยใน ADJRW จากสปสช.</<i></div>
+                        <div class="panel-heading"><i class="glyphicon glyphicon-user"></i>รายงานข้อมูลE-Claim ผู้ป่วยใน ADJRW จากสปสช.ไตรมาส1</<i></div>
                         <div class="panel-body"> 
                             <div>
 <?php
@@ -23,21 +23,21 @@ echo GridView::widget([
         'filterModel' => $searchModel,
         'panel' => [
             'before'=>'<a>รายงานข้อมูลE-Claim ผู้ป่วยใน ADJRW จากสปสช.  ประจำเดือน</a> '.date('Y-m'),
-            'after'=>'ประมวลผลปีงบประมาณ2563 จาก 1 ตุลาคม 2562  ถึง  1 ธันวาคม 2563',
+            'after'=>'<a>ประมวลผลปีงบประมาณ2563 จาก 1 ตุลาคม 2562  ถึง  1 ธันวาคม 2563</a>',
             ],
             'showPageSummary' => true,
             'columns' => [
                    // ['class' => 'yii\grid\SerialColumn'],
                     ['class' => 'kartik\grid\SerialColumn'],
-                    [
-                        'attribute' => 'HCODE',
-                        'label'=>'รหัส รพ.',
-                        'headerOptions'=>[ 'style'=>'background-color:#08EC54'] ,
-                       'format'=>'raw',
-                       'value' => function ($model, $key, $index, $widget) {
-                        return "<font  color='2E86C1'>" . $model['HCODE'] . "</font>"; 
-                },       
-                    ],
+                //     [
+                //         'attribute' => 'HCODE',
+                //         'label'=>'รหัส รพ.',
+                //         'headerOptions'=>[ 'style'=>'background-color:#08EC54'] ,
+                //        'format'=>'raw',
+                //        'value' => function ($model, $key, $index, $widget) {
+                //         return "<font  color='2E86C1'>" . $model['HCODE'] . "</font>"; 
+                // },       
+                //     ],
                     [
                         'attribute' => 'NAME',
                         'label'=>'สิทธิการพยาบาล',
@@ -79,28 +79,35 @@ echo GridView::widget([
                     ]);
                       ?>       
     </div>   
-    <div>   
+    </div>
+    </div>   
+    </div>
+    <div class="col-md-6">
+                    <div class="panel panel-info">
+                        <div class="panel-heading"><i class="glyphicon glyphicon-user"></i>รายงานข้อมูลE-Claim ผู้ป่วยใน ADJRW จากสปสช. ไตรมาส2</<i></div>
+                        <div class="panel-body"> 
+                            <div>   
 <?php
 echo GridView::widget([
         'dataProvider' => $dataProvider2,
         'filterModel' => $searchModel,
         'panel' => [
             'before'=>'<a>รายงานข้อมูลE-Claim ผู้ป่วยใน ADJRW จากสปสช.  ประจำเดือน</a> '.date('Y-m'),
-            'after'=>'ประมวลผลปีงบประมาณ2563 จาก 1 ตุลาคม 2562  ถึง  1 ธันวาคม 2563',
+            'after'=>'<a>ประมวลผลปีงบประมาณ2563 จาก 1 กุมภาพันธ์ 2563  ถึง  31 พฤษภาคม 2563</a>',
             ],
             'showPageSummary' => true,
             'columns' => [
                    // ['class' => 'yii\grid\SerialColumn'],
                     ['class' => 'kartik\grid\SerialColumn'],
-                    [
-                        'attribute' => 'HCODE',
-                        'label'=>'รหัส รพ.',
-                        'headerOptions'=>[ 'style'=>'background-color:#85C1E9'] ,
-                       'format'=>'raw',
-                       'value' => function ($model, $key, $index, $widget) {
-                        return "<font  color='2E86C1'>" . $model['HCODE'] . "</font>"; 
-                },       
-                    ],
+                //     [
+                //         'attribute' => 'HCODE',
+                //         'label'=>'รหัส รพ.',
+                //         'headerOptions'=>[ 'style'=>'background-color:#85C1E9'] ,
+                //        'format'=>'raw',
+                //        'value' => function ($model, $key, $index, $widget) {
+                //         return "<font  color='2E86C1'>" . $model['HCODE'] . "</font>"; 
+                // },       
+                //     ],
                     [
                         'attribute' => 'NAME',
                         'label'=>'สิทธิการพยาบาล',
@@ -142,29 +149,35 @@ echo GridView::widget([
                     ]);
                       ?>
                     </div> 
-                  
-   <div>    
+               </div> 
+          </div>            
+   </div>  
+   <div class="col-md-6">
+                    <div class="panel panel-info">
+                        <div class="panel-heading"><i class="glyphicon glyphicon-user"></i>รายงานข้อมูลE-Claim ผู้ป่วยใน ADJRW จากสปสช. ไตรมาส3</<i></div>
+                        <div class="panel-body"> 
+                            <div>     
 <?php
 echo GridView::widget([
         'dataProvider' => $dataProvider3,
         'filterModel' => $searchModel,
         'panel' => [
             'before'=>'<a>รายงานข้อมูลE-Claim ผู้ป่วยใน ADJRW จากสปสช.  ประจำเดือน</a> '.date('Y-m'),
-            'after'=>'ประมวลผลปีงบประมาณ2563 จาก 1 ตุลาคม 2562  ถึง  1 ธันวาคม 2563',
+            'after'=>'<>ประมวลผลปีงบประมาณ2563 จาก 1 มิถุนายน 2563  ถึง  30 กันยายน 2563</a>',
             ],
             'showPageSummary' => true,
             'columns' => [
                    // ['class' => 'yii\grid\SerialColumn'],
                     ['class' => 'kartik\grid\SerialColumn'],
-                    [
-                        'attribute' => 'HCODE',
-                        'label'=>'รหัส รพ.',
-                        'headerOptions'=>[ 'style'=>'background-color:#FADBD8'] ,
-                       'format'=>'raw',
-                       'value' => function ($model, $key, $index, $widget) {
-                        return "<font  color='2E86C1'>" . $model['HCODE'] . "</font>"; 
-                },       
-                    ],
+                //     [
+                //         'attribute' => 'HCODE',
+                //         'label'=>'รหัส รพ.',
+                //         'headerOptions'=>[ 'style'=>'background-color:#FADBD8'] ,
+                //        'format'=>'raw',
+                //        'value' => function ($model, $key, $index, $widget) {
+                //         return "<font  color='2E86C1'>" . $model['HCODE'] . "</font>"; 
+                // },       
+                   // ],
                     [
                         'attribute' => 'NAME',
                         'label'=>'สิทธิการพยาบาล',
